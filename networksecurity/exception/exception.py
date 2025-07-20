ff7d85cda2cbb5d23e9b6011c569adc81aa0e8c6
KundrapuHarika124 <harikakundrapu216@gmail.com>
2025-07-20 22:53:03 +0530

    Exception handling

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,19 @@
+import sys
+from networksecurity.logging import logger
+class NetworkSecurityException(Exception):
+    """Base class for all network security exceptions."""
+    def __init__(self, error_message, error_details: sys):
+        self.error_message = error_message
+        _, _, exc_tb = error_details.exc_info()
+
+        if exc_tb is not None:
+            self.file_name = exc_tb.tb_frame.f_code.co_filename
+            self.line_no = exc_tb.tb_lineno
+        else:
+            self.file_name = "Unknown"
+            self.line_no = -1
+
+    def __str__(self):
+        return f"Error occurred in file {self.file_name} at line {self.line_no}: {self.error_message}"
+        self.file_name,self.line_no ,str(self.error_message)
+
